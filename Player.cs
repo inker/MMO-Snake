fc50aca966f15293722e188a5252cef7540fe706
inker <anton.veselyev@gmail.com>
2015-12-15 00:34:12 +0300

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Player
+    {
+        public List<Vec2> Snake;
+        public int ColorNum;
+        public int Score;
+        public Player(List<Vec2> snake, int colorNum, int score = 0)
+        {
+            Snake = snake;
+            ColorNum = colorNum;
+            Score = score;
+        }
+
+        public Player(int score = 0) {
+            Score = score;
+        }
+    }
+}
---
+++ Vec2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    public class Vec2
+    {
+        public int X { get; set; } // X Coordinate for Snake Part
+        public int Y { get; set; } // Y Coordinate for Snake Part
+
+        // Constructor
+        public Vec2()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Vec2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
